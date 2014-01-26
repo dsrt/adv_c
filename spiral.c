9b7c5343b3eede393f727df7ce059c51ac2209f2
dsrt <sreeravitheja@gmail.com>
2014-01-26 14:19:10 +0530

    Spiral Fill

--- spiral.c
+++ spiral.c
@@ -1,13 +1,62 @@
 #include<stdio.h>
 
-main()
+void spiral(int a[30][30], int l, int no, int n)
 {
-	int a[10][10], l, i, j, n=0, no=0;
+	int i=0;
 	
-	printf("Enter the length of the row: %d");
+	for(i=0; i<l; i++)
+	{
+		no++;	
+		if(i<l-1)
+		{
+			a[n][i+n]=no;
+			a[i+n][l+n-1]=no+l-1;	
+			a[l+n-1][l+n-i-1]=no+l+l-2;
+			a[l+n-i-1][n]=no+l+l+l-3;
+		}
+
+		else
+		{
+			a[n][i+n]=no;
+			a[i+n][l+n-1]=no+l-1;	
+			a[l+n-1][l+n-i-1]=no+l+l-2;
+		}	
+	}	
+}
+
+int main()
+{
+	int a[30][30], l, i, j, no=0, n=0;
+	
+	printf("Enter the length of the row: ");
 	scanf("%d", &l);
+
+	for(i=l; i>0; i-=2)
+	{
+		spiral(a, i, no, n);
+		n++;
+		no=no+4*(i-1);
+	}
 
 	for(i=0; i<l; i++)
 	{
-		
+		printf("\n");
+		for(j=0; j<l; j++)
+		{
+			if(a[i][j]/10>=10)			
+				printf(" %d", a[i][j]);
+			else if(a[i][j]/10==0)
+				printf("   %d", a[i][j]);
+			else
+				printf("  %d", a[i][j]);
+		}
+	}
+
+	return 0;
+}		
+
+
+
+
+
 		
